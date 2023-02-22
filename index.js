45aba2743fea8c639fd9ea4a95ceed20713e6cbf
GuerayK <62020082+GuerayK@users.noreply.github.com>
2023-02-23 00:09:29 +0100

    added EventListeners to buttons

---
+++ index.js
@@ -0,0 +1,9 @@
+let numberOfButtons = document.querySelectorAll(".drum").length;
+
+for (let i = 0; i < numberOfButtons; i++){
+
+    document.querySelectorAll(".drum")[i].addEventListener("click", function(){
+        alert("I got clicked");
+    });
+
+};
